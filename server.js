bcbad6bbc7ffa99497ca43f6ba4ffda1bc29c616
Christian Duvholt <christian@iterate.no>
2019-02-06 14:07:26 +0100

    Add Google Auth using passport

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 /* eslint-disable no-console */
 
 const express = require("express");
+const { initialize } = require("./auth");
 
 const app = express();
 
 app.use(express.static("dist"));
+
+initialize(app);
+
+app.get("/", (req, res) => {
+  res.send(`<a href="/auth/google">Sign In with Google</a>`);
+});
 
 app.listen(1234);
 
