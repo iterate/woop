11e3e6f335f55e6e8699b43634a47af79d5fec1a
Ole Martin <OIex@users.noreply.github.com>
2019-02-06 14:15:20 +0100

    Initial feed (#1)
    * Better config
    
    * Initial app structure
    
    * Cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
 import React from "react";
+import styled from "styled-components";
 
-const App = () => <p>Woop</p>;
+import Header from "@/components/Header";
+import Editor from "@/components/Editor";
+import Feed from "@/components/Feed";
+
+const AppContainer = styled.div`
+  background-color: #fafafa;
+  color: #111;
+  min-height: 100vh;
+  padding: 0 30px;
+`;
+
+const App = () => (
+  <AppContainer>
+    <Header />
+    <Editor />
+    <Feed />
+  </AppContainer>
+);
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+const HeaderContainer = styled.div``;
+
+const Header = () => <HeaderContainer>Woop Header </HeaderContainer>;
+
+export default Header;
