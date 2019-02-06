d66d0ac6b000868e6d371adee4899d24b314e9de
Boye Borg <boye.borg@icloud.com>
2019-02-06 16:39:59 +0100

    update editor and use cool button

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,37 +1,32 @@
 import React from "react";
 import styled from "styled-components";
 
-const EditorContainer = styled.div``;
-const EditorInput = styled.textarea`
-  width: 100%;
-  height: 300px;
-  border-radius: 4px;
-  border: solid 2px #ccc;
-  padding: 8px;
-  box-sizing: border-box;
-  :focus {
-    outline: none !important;
-    background-color: black;
-    color: white;
+import Button from "@/components/Button";
+
+const EditorContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  & > *:not(:first-child) {
+    margin-top: 10px;
   }
 `;
-const Submit = styled.button`
-  float: right;
-  color: #111;
-  background-color: #aaccff;
-  padding: 4px 8px;
-  border-radius: 40px;
-  border: 1px solid #aaccff;
+const EditorInput = styled.textarea`
+  height: 300px;
+  border: solid 1px #aaa;
+  padding: 3px;
+
   :focus {
-    outline: none !important;
-    border: 1px solid black;
+    outline: none;
+    background-color: black;
+    color: white;
   }
 `;
 
 const Editor = () => (
   <EditorContainer>
     <EditorInput placeholder="<Woop... />" />
-    <Submit>Submit</Submit>
+    <Button>Submit</Button>
   </EditorContainer>
 );
 
