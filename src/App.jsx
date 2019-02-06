7547987428e3c9fe9fbb30fa4a8b7866d2787d56
Boye Borg <boye.borg@icloud.com>
2019-02-06 16:39:34 +0100

    add global styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 
 import Header from "@/components/Header";
 import Editor from "@/components/Editor";
 import Feed from "@/components/Feed";
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #fafafa;
+    color: #111;
+    box-sizing: border-box;
+  }
+`;
+
 const AppContainer = styled.div`
-  background-color: #fafafa;
-  color: #111;
   min-height: 100vh;
   padding: 0 30px;
+
+  & > *:not(:first-child) {
+    margin-top: 20px;
+  }
 `;
 
 const App = () => (
   <AppContainer>
+    <GlobalStyle />
     <Header />
     <Editor />
     <Feed />
