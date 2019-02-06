5645cf15d44701df47840c5e1d0fb67acfc5c2a0
Ole Martin <OIex@users.noreply.github.com>
2019-02-06 15:41:38 +0100

    Initial editor (#5)
    * Better config
    
    * Initial app structure
    
    * Cleanup
    
    * Basic styling of editor
    
    * Basic header stuff

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import styled from "styled-components";
 
-const HeaderContainer = styled.div``;
+const HeaderContainer = styled.div`
+  display: flex;
+  flex: 1;
+  height: 30px;
+  padding: 4px 0;
+  justify-content: space-between;
+  align-items: center;
+`;
+const LogOut = styled.button`
+  color: blue;
+  background-color: transparent;
+  border: none;
+`;
+const Logo = styled.img`
+  height: 30px;
+`;
 
-const Header = () => <HeaderContainer>Woop Header </HeaderContainer>;
+const Header = () => (
+  <HeaderContainer>
+    <Logo src="https://www.iterate.no/favicon.a7c7b321.png" alt="Logo" />
+    <LogOut>Logg ut</LogOut>
+  </HeaderContainer>
+);
 
 export default Header;
