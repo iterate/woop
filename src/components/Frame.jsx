74f2b836059b444bf07726f1b653634410491ac5
Boye Borg <boye.borg@icloud.com>
2019-02-06 15:38:26 +0100

    remove prop-types

--- src/components/Frame.jsx
+++ src/components/Frame.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { createPortal } from "react-dom";
-import PropTypes from "prop-types";
 import styled from "styled-components";
 
 const Iframe = styled.iframe`
@@ -19,11 +18,6 @@ const Frame = ({ id, html }) => {
       {mountNode && createPortal(frameBody, mountNode)}
     </Iframe>
   );
-};
-
-Frame.propTypes = {
-  id: PropTypes.string.isRequired,
-  html: PropTypes.string.isRequired
 };
 
 export default Frame;
