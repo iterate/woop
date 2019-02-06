5500a81939b59cf03c8c0c62c4b98ac3f4707cba
Ole Martin Knurvik <ole.martin.knurvik@iterate.no>
2019-02-06 14:10:17 +0100

    Initial app structure

---
+++ src/components/Editor.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const EditorContainer = styled.div``;
+
+const Editor = () => (
+  <EditorContainer>
+    <input />
+  </EditorContainer>
+);
+
+export default Editor;
---
+++ src/components/Feed.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+const FeedContainer = styled.div``;
+
+const Feed = () => <FeedContainer>Woop feed </FeedContainer>;
+
+export default Feed;
