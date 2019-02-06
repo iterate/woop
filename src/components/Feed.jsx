c2da470ea49071e2c21487ac929cc7e1cbfba18d
Ole Martin Knurvik <ole.martin.knurvik@iterate.no>
2019-02-06 15:56:29 +0100

    Post renders title and content

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import styled from "styled-components";
 
+import Post from "@/components/Post";
+
 const FeedContainer = styled.div``;
 
-const Feed = () => <FeedContainer>Woop feed </FeedContainer>;
+const Feed = () => (
+  <FeedContainer>
+    <Post
+      user={{
+        name: "Test Testman",
+        image:
+          "https://s3.amazonaws.com/fixd-wordpress-cdn/wp-content/uploads/2018/04/Picture-of-person.png"
+      }}
+      post={{ id: "123", html: "<p>test text</p>" }}
+    />
+  </FeedContainer>
+);
 
 export default Feed;
