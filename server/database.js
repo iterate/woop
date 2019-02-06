cc52444cf0e214b5b15379b4acbd2ddeb2a77d5e
Christian Duvholt <christian@iterate.no>
2019-02-06 15:48:18 +0100

    Store user in database on login

---
+++ server/database.js
@@ -0,0 +1,19 @@
+import Sequelize from "sequelize";
+
+export const sequelize = new Sequelize("database", "username", "password", {
+  dialect: "sqlite",
+  // SQLite only
+  storage: "./database.sqlite",
+
+  // http://docs.sequelizejs.com/manual/tutorial/querying.html#operators
+  operatorsAliases: false
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch(err => {
+    console.error("Unable to connect to the database:", err);
+  });
