fad40df66fb6b00085820ffb63d9e67c3c15f72f
Ole Martin Knurvik <ole.martin.knurvik@iterate.no>
2019-02-06 14:55:11 +0100

    Basic styling of editor

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -2,10 +2,36 @@ import React from "react";
 import styled from "styled-components";
 
 const EditorContainer = styled.div``;
+const EditorInput = styled.textarea`
+  width: 100%;
+  height: 300px;
+  border-radius: 4px;
+  border: solid 2px #ccc;
+  padding: 8px;
+  box-sizing: border-box;
+  :focus {
+    outline: none !important;
+    background-color: black;
+    color: white;
+  }
+`;
+const Submit = styled.button`
+  float: right;
+  color: #111;
+  background-color: #aaccff;
+  padding: 4px 8px;
+  border-radius: 40px;
+  border: 1px solid #aaccff;
+  :focus {
+    outline: none !important;
+    border: 1px solid black;
+  }
+`;
 
 const Editor = () => (
   <EditorContainer>
-    <input />
+    <EditorInput placeholder="<Woop... />" />
+    <Submit>Submit</Submit>
   </EditorContainer>
 );
 
