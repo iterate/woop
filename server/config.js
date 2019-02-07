c2e465014b37f92bb7b29bd9dbbcea682f33d1f5
Christian Duvholt <christian@iterate.no>
2019-02-07 09:16:44 +0100

    Add config file

---
+++ server/config.js
@@ -0,0 +1,5 @@
+export default {
+  client_id: process.env.GOOGLE_CLIENT_ID,
+  client_secret: process.env.GOOGLE_CLIENT_SECRET,
+  database_url: "sqlite://database.sqlite" // process.env.DATABASE_URL
+};
