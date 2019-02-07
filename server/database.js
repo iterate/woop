94c8dde83c9b0754bb4e4ea8daee57e3b2bb1ed0
Christian Duvholt <christian@iterate.no>
2019-02-07 09:10:43 +0100

    Read config from env vars

--- server/database.js
+++ server/database.js
@@ -1,13 +1,7 @@
 import Sequelize from "sequelize";
+import config from "./config";
 
-const sequelize = new Sequelize("database", "username", "password", {
-  dialect: "sqlite",
-  // SQLite only
-  storage: "./database.sqlite",
-
-  // http://docs.sequelizejs.com/manual/tutorial/querying.html#operators
-  operatorsAliases: false
-});
+const sequelize = new Sequelize(config.database_url);
 
 sequelize
   .authenticate()
