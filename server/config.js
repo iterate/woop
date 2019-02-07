988a7082388493143d13019b5b65c5bd09b159ea
Christian Duvholt <christian@iterate.no>
2019-02-07 09:21:57 +0100

    Add defaults

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 export default {
-  client_id: process.env.GOOGLE_CLIENT_ID,
-  client_secret: process.env.GOOGLE_CLIENT_SECRET,
+  client_id: process.env.GOOGLE_CLIENT_ID || "test",
+  client_secret: process.env.GOOGLE_CLIENT_SECRET || "test",
   database_url: "sqlite://database.sqlite" // process.env.DATABASE_URL
 };
