8a71b27faaa3bed38ea46b46892f200f7b9b0523
Christian Duvholt <christian@iterate.no>
2019-02-07 10:18:14 +0100

    Disable XSS on preview

--- server/api/preview.js
+++ server/api/preview.js
@@ -3,6 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", async ({ query: { post } }, res) => {
+  res.setHeader("X-XSS-Protection", "0");
   res.send(post);
 });
 
